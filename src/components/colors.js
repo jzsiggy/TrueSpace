70b1eb48b08849c5c874ef791aa65bb44b334041
Joao Zsigmond <jzsiggy@gmail.com>
2020-05-24 02:59:26 -0300

    Adding palette

---
+++ src/components/colors.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Blue = styled.span`
+  color: #6fbfc1;
+`
+
+const Brown = styled.span`
+  color: #46382a;
+`
+
+const Yellow = styled.span`
+  color: #f4af31;
+`
+
+export {
+  Blue, 
+  Brown, 
+  Yellow,
+};
