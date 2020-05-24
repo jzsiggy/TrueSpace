987905dcc1575e8529ec7cc9dfea69bc56772766
Joao Zsigmond <jzsiggy@gmail.com>
2020-05-24 02:56:34 -0300

    Adding simvals

---
+++ src/seedDB.js
@@ -0,0 +1,23 @@
+const user = {
+  nome: 'João Zsigmond',
+
+  cpf: '475.359.578-11',
+
+  idade: 19,
+
+  probCovid: '23',
+
+  sintomas: ['dorCabeca', 'tosse', 'febre', 'pupilaBranca'],
+  
+  sintCancer : {
+    'Câncer de Pele' : ['pupilaBranca', 'dorCabeca'],
+    'Leucemia' : ['febre']
+  }
+  // :
+  // :
+  // :
+};
+
+export {
+  user,
+};
